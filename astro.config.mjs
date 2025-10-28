4ed59009a3a1a12f9867e92f9bc66ceb58cfef17
Wilson Gutierrez <adolfoguti001@outlook.com>
2025-10-28 08:15:51 -0600

    change default export to '@'

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,17 @@
 // @ts-check
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
+import { fileURLToPath, URL } from 'node:url';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      }
+      }
   },
 });
   

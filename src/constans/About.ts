3b30ef801f5a85a64361968199a65d9f8789a784
wgutierrez <adolfoguti001@outlook.com>
2025-11-28 18:33:05 -0600

    fea: responsive has been added

---
+++ src/constans/About.ts
@@ -0,0 +1,58 @@
+import Experiencia from "@/assets/about/experiencia.svg";
+import Mision from "@/assets/about/mision.svg";
+import Logros from "@/assets/about/logros.svg";
+import Valores from "@/assets/about/valores.svg";
+
+export const about = {
+  experiencia: {
+    key: "experiencia",
+    title: "Experiencia",
+    icon: Experiencia,
+    description: `<p>AXIS-MED nace a partir de la experiencia de más de 30 años en el campo del Diagnóstico por Imágenes y otras áreas 
+del cuidado de la Salud a través de la tecnología.</p>
+<p>Experiencia adquirida y aplicada en el sector de la Salud en El Salvador, en Centroamérica, Latinoamérica y en 
+ciertos países del mundo.</p>
+<p>Experiencia en proyectos y servicios de distintas modalidades de Diagnóstico por imágenes.</p>`,
+    bgColor: "#2583C5",
+    visible: false,
+  },
+  mision: {
+    key: "mision",
+    title: "Misión",
+    icon: Mision,
+    bgColor: "#4690BE",
+    description: `Aportar toda nuestra experiencia y conocimiento de forma confiable a todos aquellos que proveen servicios de salud por medio de la tecnología médica en condiciones accesibles y flexibles.`,
+    visible: false,
+  },
+  valores: {
+    key: "valores",
+    title: "Valores",
+    icon: Valores,
+    bgColor: "#679CB7",
+    description: `<p>Agradecimiento a Dios porque nos permite aportar valor a los sistemas de salud, a nuestros clientes y a nuestros colaboradores.</p>
+
+<p>Pasión por la Innovación Tecnológica en la Salud.</p>
+
+<p>Establecer relaciones transparentes.</p>
+
+<p>Confiabilidad en lo que hacemos y ofrecemos.</p>
+
+<p>Insistimos en ofrecer el mejor servicio a nuestros clientes.</p>`,
+
+    visible: false,
+  },
+  logros: {
+    key: "logros",
+    title: "Logros",
+    icon: Logros,
+    bgColor: "#82ADC4",
+    description: `<p>Celebración de grandes victorias y momentos memorables.</p>
+
+<p>Proveedores Confiables.</p>
+
+<p>Posicionamiento en Ultrasonografía y Veterinaria.</p>
+
+<p>Cumplimiento de Regulaciones Sanitarias.</p>`,
+    visible: false,
+  },
+};
---
+++ src/constans/Products.ts
@@ -0,0 +1,20 @@
+export const products = [
+  {
+    key: "equipo-medico",
+    title: "Diagnostico por Imagen",
+    img: "/img/solutions/diagnostico.webp",
+    link: "/diagnosis",
+  },
+  {
+    key: "instrumental",
+    title: "Instrumental Quirúrgico",
+    img: "/img/solutions/instrumental.webp",
+    link: "/surgery",
+  },
+  {
+    key: "veterinaria",
+    title: "Veterinaria",
+    img: "/img/solutions/veterinario.webp",
+    link: "/veterinary",
+  },
+];

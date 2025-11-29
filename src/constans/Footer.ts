abcc579a0ba97fcb5e07931ac0592c89e2ab6b4a
wgutierrez <adolfoguti001@outlook.com>
2025-11-29 10:25:12 -0600

    feat: map has been added

---
+++ src/constans/Footer.ts
@@ -0,0 +1,35 @@
+import FacebookIcon from "@/assets/social/facebook.svg";
+import GmailIcon from "@/assets/social/gmail.svg";
+import InstagramIcon from "@/assets/social/instagram.svg";
+import PhoneIcon from "@/assets/social/phone.svg";
+
+export const links = [
+  { href: "/#about", text: "Sobre nosotros" },
+  { href: "/#brands", text: "Nuestras Marcas" },
+  { href: "/#services", text: "Servicios" },
+  { href: "/#mapView", text: "Encuéntranos" },
+  { href: "/#faq", text: "Preguntas Frecuentes" },
+];
+
+export const socialMedia = [
+  {
+    platform: "Facebook",
+    url: "https://www.facebook.com/profile.php?id=61572627796132",
+    icono: FacebookIcon,
+  },
+  {
+    platform: "Instagram",
+    url: "https://www.instagram.com/axismed.sv/",
+    icono: InstagramIcon,
+  },
+  {
+    platform: "axismed.info@gmail.com",
+    url: "mailto:axismed.info@gmail.com",
+    icono: GmailIcon,
+  },
+  {
+    platform: "+503 77422509",
+    url: "/",
+    icono: PhoneIcon,
+  },
+];
